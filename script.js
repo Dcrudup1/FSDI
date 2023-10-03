93f8e77e08089ed32f0f41e75efc36152c2cd532
Dcrudup1 <144864820+Dcrudup1@users.noreply.github.com>
2023-10-03 07:48:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+//*Assignment*//
+
+//*Variables*//
+let fName ="Dedrick"
+let lName =" Crudup "
+let greeting= "Hello,"
+let theOpener= "Thank you for the application"
+let pNumber= " phone number 661-917-4467"
+let eMail= " dedrickcrudupj@gmail.com"
+let SAddress= " 215 W 6th st "
+let Zpicode=" 90014"
+let myCity= " los Angeles"
+let myAge= " 24"
+let theDate=" 10/2/2023"
+let Time= " 3:00PM"
+let houseMembers=" 4 household members"
+let dEvices=" 7 devices connected"
+let myState=" california"
+let mySocial= " 111-22-3333"
+let Refernce1=" Ikeyla Fuller #661-000-000"
+let refernce2= " Tierra Fuller #661-000-000"
+let myBirthday= "01/29/1999"
+let Closer= "Thank you have a very best day!!"
+
+
+
+
+console.log( greeting + theOpener + " my name is " + fName   + lName)
+console.log("Here is my " + pNumber)
+console.log( " Along with my email" + eMail )
+console.log( "and  address" + SAddress + myCity+ myState+Zpicode)
+console.log("my birthday is " + myBirthday)
+console.log("I am" +  myAge)
+console.log(" I am looking to change phone carries for," + houseMembers+ " Using" + dEvices)
+console.log("Here are my 2 added names to my account along with there number")
+console.log(Refernce1)
+console.log(refernce2)
+console.log("I completed this application on" + theDate + "at" + Time)
+console.log(" I am looking forward to getting this account setup, And cant wait for the faster service!!")
+console.log(Closer)
